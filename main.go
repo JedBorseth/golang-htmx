8abffd4d4e936bf71cffcc4465c80a7794fee386
Jed Borseth <jedborseth@Jeds-MBP.va.shawcable.net>
2023-11-15 20:50:23 -0800

    ui for / page

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+	  log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	r := gin.Default()
@@ -12,10 +26,26 @@ func main() {
 		})
 	})
 	r.GET("/", func(c *gin.Context) {
-		data := make(map[string]interface{})
-		data["title"] = "Home Page"
-		data["body"] = "This is the home page"
-		homePage(c,  data)
+		homePage(c)
 	})
+	r.GET("/style.css", func(c *gin.Context) {
+		c.File("styles/output.css")
+	})
+	// Get all files in the assets folder
+	assetsDir := "./assets"
+	files, err := os.ReadDir(assetsDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Make a GET endpoint for each file
+	for _, file := range files {
+		if !file.IsDir() {
+			filePath := filepath.Join(assetsDir, file.Name())
+			r.GET("/"+file.Name(), func(c *gin.Context) {
+				http.ServeFile(c.Writer, c.Request, filePath)
+			})
+		}
+	}
+	
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
--- parseTemplates.go
+++ parseTemplates.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 
 	"github.com/gin-gonic/gin"
 )
-
-func homePage(c *gin.Context, content any) {
+ 
+func header(c *gin.Context, content any) {
 	templ, err := template.ParseFiles("templates/header.html")
+		if(err != nil) { 
+		fmt.Print("Error parsing row template\n ", err)
+		return
+		} 
+		templ.Execute(c.Writer, content)
+}
+
+func homePage(c *gin.Context ) {
+	data := make(map[string]interface{})
+	buf := new(bytes.Buffer)
+	data["title"] = "Home Page"
+
+	templ, err := template.ParseFiles("templates/home.html")
 		if(err != nil) {
 		fmt.Print("Error parsing row template\n ", err)
 		return
 		}
-		templ.Execute(c.Writer, content)
+	templ.Execute(buf, data)
+	data["body"] = template.HTML(buf.String())
+	header(c, data)
+	
 }
